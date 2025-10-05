65f150656f30e1cb68c20349e6c5aa049cf915f4
Cristian-Massa <cristianmassa30@gmail.com>
2025-10-05 19:01:09 -0300

    initial set up for web app

---
+++ app/src/core/components/providers.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { PropsWithChildren, useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+export const Providers = ({ children }: PropsWithChildren) => {
+  const [queryClient] = useState(() => new QueryClient());
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>
+        {children}
+        <ReactQueryDevtools initialIsOpen={true} />
+      </QueryClientProvider>
+    </>
+  );
+};
